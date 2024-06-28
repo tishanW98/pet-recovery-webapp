503dbd4a7f0ed9d168d83658b9d88f5fff04cbb5
tishanW98 <shakilatishan1998@outlook.com>
2024-06-29 00:53:46 +0530

    backend-updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,40 @@
-require('dotenv').config()
-const express = require('express') //done
-const app = express()  //done
-const createHttpError = require('http-errors')
-const foundPetPersonRouter = require('./routes/foundPetPerson')
-const lostPetOwnerRouter = require('./routes/lostPetOwner')
-const petRouter = require('./routes/pet')
-const foundpetRouter = require('./routes/foundPet')
-const fileUpload = require('express-fileupload');
+require("dotenv").config();
+const express = require("express"); //done
+const app = express(); //done
+const createHttpError = require("http-errors");
+const foundPetPersonRouter = require("./routes/foundPetPerson");
+const lostPetOwnerRouter = require("./routes/lostPetOwner");
+const petRouter = require("./routes/pet");
+const foundpetRouter = require("./routes/foundPet");
+const fileUpload = require("express-fileupload");
 
 app.use(fileUpload());
 
-app.use('/public/pets', express.static('public/pets'))
+app.use("/public/pets", express.static("public/pets"));
 
 //cors
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1/foundPetPersons', foundPetPersonRouter);
-app.use('/api/v1/lostPetOwners', lostPetOwnerRouter); 
-app.use('/api/v1/pets', petRouter); 
-app.use('/api/v1/foundpets', foundpetRouter); 
+app.use("/api/v1/foundPetPersons", foundPetPersonRouter);
+app.use("/api/v1/lostPetOwners", lostPetOwnerRouter);
+app.use("/api/v1/pets", petRouter);
+app.use("/api/v1/foundpets", foundpetRouter);
 
-app.use((err,req,res, next) => {
-  if (createHttpError.isHttpError(err)){
-    res.status(err.status).send({message: err.message})
-  }else{
-    res.status(500).send({message: err.message})
+app.get("/", (req, res) => {
+  res.send("Backend connected to localhost now testing .");
+});
+
+app.use((err, req, res, next) => {
+  if (createHttpError.isHttpError(err)) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    res.status(500).send({ message: err.message });
   }
   //res.status(500).send({message: "Error Unknown"})
-  
-}); 
+});
 
 //app.post('/api/v1/foundPetPersons', )
 
