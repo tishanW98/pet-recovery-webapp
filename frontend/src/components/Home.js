3973fd6eba21b335ae6d0f8a05c722145064bb35
tishanW98 <shakilatishan1998@outlook.com>
2024-06-29 01:45:59 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -69,7 +69,7 @@ const Home = () => {
             component={Link}
             to="/signin"
           >
-            I Lost My Pet
+             My Pet
           </Button>
           {/* Additional paragraph under the buttons */}
           <Typography variant="body1" style={{ marginTop: '20px', textAlign: 'center' }}>
